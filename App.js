10d6875106f2efd27cbb4b0b038c060a9137030f
HASSANI Fatima Zahra <hassani.fatimazahera199@gmail.com>
2021-07-07 19:08:22 +0100

    start

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log("app");
+  return (
+    <View style={styles.container}>
+      <Text>np_mobileapp</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
