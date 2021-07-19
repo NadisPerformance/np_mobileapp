d91d2990fd2eb2421c6b6b6cdb5687b8661ee9dd
HASSANI Fatima Zahra <hassani.fatimazahera199@gmail.com>
2021-07-19 12:37:07 +0100

    second commit

---
+++ src/APropos.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+const SecondPage = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+        </View>
+        <Text style={{ fontSize: 16, textAlign: 'center', color: 'grey' }}>
+        http://nadisperformance.com
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default SecondPage;
---
+++ src/Accueil.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {  View, Text, SafeAreaView } from 'react-native';
+const FirstPage = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+        </View>
+        <Text style={{ fontSize: 18, textAlign: 'center', color: 'grey' }}>
+        </Text>
+        <Text style={{ fontSize: 16, textAlign: 'center', color: 'grey' }}>
+        http://nadisperformance.com
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default FirstPage;
